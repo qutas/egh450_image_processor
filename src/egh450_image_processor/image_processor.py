901b5647c4bb79f6350786ae9d81f262212c8c68
pryre.dev@outlook.com <pryre.dev@outlook.com>
2018-08-22 12:24:18 +1000

    Added parameter for loading cascade training

--- src/egh450_image_processor/image_processor.py
+++ src/egh450_image_processor/image_processor.py
@@ -7,12 +7,14 @@ from std_msgs.msg import String
 from sensor_msgs.msg import CompressedImage
 from cv_bridge import CvBridge, CvBridgeError
 
-# will need to change the location to your own environment
-sign_cascade = cv2.CascadeClassifier('/home/kyle/catkin_ws/src/egh450_image_processor/data/cascade.xml')
-
 class ImageProcessor():
 	def __init__(self):
-		#Set up the CV Bridge
+		# Get the path to the cascade XML training file using ROS parameters
+		# Then load in our cascade classifier
+		sign_cascade_file = str(rospy.get_param("~cascade_file"))
+		self.sign_cascade = cv2.CascadeClassifier(sign_cascade_file)
+
+		# Set up the CV Bridge
 		self.bridge = CvBridge()
 
 		# Set up the subscriber
@@ -24,7 +26,7 @@ class ImageProcessor():
 		self.sub_img.unregister()
 
 	def callback_img(self, msg_in):
-		#Convert ROS image to CV image
+		# Convert ROS image to CV image
 		try:
 			cv_image = self.bridge.compressed_imgmsg_to_cv2( msg_in, "bgr8" )
 		except CvBridgeError as e:
@@ -34,15 +36,14 @@ class ImageProcessor():
 		# Do processing here!
 		# ===================
 		gray = cv2.cvtColor(cv_image, cv2.COLOR_BGR2GRAY)
-		
-		sign = sign_cascade.detectMultiScale(gray, 1.01, 1, minSize=(100,100))
-		
+
+		sign = self.sign_cascade.detectMultiScale(gray, 1.01, 1, minSize=(100,100))
+
 		for (x,y,w,h) in sign:
 			cv2.rectangle(cv_image,(x,y),(x+w,y+h),(255,0,0),2)
-		
 		# ===================
 
-		#Convert CV image to ROS image and publish
+		# Convert CV image to ROS image and publish
 		try:
 			self.pub_img.publish( self.bridge.cv2_to_compressed_imgmsg( cv_image ) )
 		except CvBridgeError as e:
